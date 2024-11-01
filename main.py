d9feddc5fe8e99b151d8c736348f4091877cd786
David Hochstetler <daviddevs23@gmail.com>
2024-11-01 08:06:23 -0400

    Fixed weird routing

--- main.py
+++ main.py
@@ -2,19 +2,21 @@ from nicegui import ui
 from pages.labels.locationGen import locationLabel
 import asyncio
 
-with ui.header().classes(replace="row items-center bg-black") as header:
-    with ui.tabs() as tabs:
-        ui.tab("Home")
-        ui.tab("Location")
+@ui.page('/')
+def home():
+    with ui.header().classes(replace="row items-center bg-black") as header:
+        with ui.tabs() as tabs:
+            ui.tab("Home")
+            ui.tab("Location")
 
-with ui.tab_panels(tabs, value="Home").classes("w-full"):
-    # Home Page
-    with ui.tab_panel("Home"):
-        ui.label("Kendrick Plastics Utilities").classes("text-4xl")
-        ui.label("If anything doesn't work, don't contact IT")
+    with ui.tab_panels(tabs, value="Home").classes("w-full"):
+        # Home Page
+        with ui.tab_panel("Home"):
+            ui.label("Kendrick Plastics Utilities").classes("text-4xl")
+            ui.label("If anything doesn't work, don't contact IT")
 
-    # Location Label Generator
-    with ui.tab_panel("Location") as locationDiv:
-        locationLabel(ui)
+        # Location Label Generator
+        with ui.tab_panel("Location") as locationDiv:
+            locationLabel(ui)
 
 ui.run(port=6969)
